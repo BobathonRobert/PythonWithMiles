fccb8e8ac6c80b281f185a0526430a52da25cf11
Bobathon Robert <pimanhappy@gmail.com>
2017-10-14 18:01:03 +1100

    Adding Files

---
+++ alfred.py
@@ -0,0 +1,410 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct  3 12:14:49 2017
+
+@author: u6049237 -0487201989
+"""
+
+
+# Weather files
+# Formatting of Weath Files
+    # Product Code
+    # Bureau of Meteorology station number
+    # Year
+    # Mnnth
+    # Day
+    # Rainfall Amount (millimeters)
+    # Period over which rainfall was measured (days)
+    # Quality
+
+#with open('Rainfall_files/Rainfall_Canberra_070247.csv', 'r') as CanberraFile:
+    
+
+#with open('Rainfall_files/Rainfall_Queanbeyan_070072.csv', 'r') as QueanbeyanFile:
+    
+
+#with open('Rainfall_files/Rainfall_Sydney_066062.csv', 'r') as SydneyFile:
+    
+WeatherStations = []
+WetOrDry = []
+climatePrint = ["driest period"]
+DifferenceValue = []
+DifferenceUnit = []
+TimePeriod = []
+TimePeriodPrint = []
+Occurences = []
+
+
+# For Miles' File-----------------------
+def avgRainAccrossDays(data):
+    myDays = data
+    
+    # index represents current day
+    index = 0
+    while index < len(data):
+        daysSpanned = myDays[index][6]
+        daysCounter = daysSpanned
+        
+        #if the period which it was measured is greater than 1
+        if daysSpanned > 1:
+            # store the rainfall accross those days, and find the avg
+            spannedRain = myDays[index][5]
+            avgRain = spannedRain / daysSpanned
+        
+            # for everyday which accumilated to the spannedRainfall value
+            # set its rainfall to the avg
+            # and its period for collecting rainfall to 1 day
+            while daysSpanned > 0:
+                myDays[index - (daysCounter-1) ][5] = avgRain
+                myDays[index - (daysCounter-1) ][6] = 1
+                daysCounter -= 1
+            
+        index += 1
+    
+    return myDays
+# General Functions----------------------
+def isValid(listOfValid, CheckThis):
+    for item in listOfValid:
+        if CheckThis.lower() == item:
+            return True
+    return False
+
+def appendFirstEle(toPrint, dates):
+    for date in dates:
+        toPrint += date[0] + "\n"
+        
+def runMiles(a, b, c, d, e, f):
+    '''
+    toPrint = "Here are the following "+ f +":\n"
+    WetDry = c
+    Miles1TotalRain = 0
+    Miles2TotalRain = 0
+    Wetter = ""
+    
+    Miles1 = miles1(a, b, c, d, e, f)
+    Miles2 = miles2(a, b, c, d, e, f)
+    
+    for date in Miles1:
+        Miles1TotalRain += date[1]
+    for date in Miles2:
+        Miles2TotalRain += date[1]
+
+    if Miles1TotalRain >= Miles2TotalRain:
+        Wetter = "Miles1"
+    else:
+        Wetter = "Miles2"
+    
+    if WetDry == "wet":
+        if "1" in Wetter:
+            appendFirstEle(toPrint, Miles1)
+        else:
+            appendFirstEle(toPrint, Miles2)
+    else:
+        if "1" in Wetter:
+            appendFirstEle(toPrint, Miles2)
+        else:
+            appendFirstEle(toPrint, Miles1)
+                
+    print(toPrint)   ''' 
+    print("hi miles")
+# Question 1 Functions----------------------
+def validAmountOfStations(number):
+    if len(number) > 1:
+        return False
+    for character in number:
+        if not ord("1") <= ord(character) <= ord("3"):
+            return False
+    return True
+def validStation(station):
+    stations = ["canberra", "sydney", "queanbeyan"]
+    return  isValid(stations, station)
+
+def addStation():
+    tempStation = input("Please enter in the Name of the Weather station you \
+wish to add: ")
+    if validStation(tempStation):
+        if not tempStation in WeatherStations:
+            WeatherStations.append(tempStation.lower())
+        else:
+            print("Although that is an actual station, you've already added \
+that one.")
+            addStation()
+    else:
+        print("Sorry, I don't think '"+ tempStation+ "' is a valid station :(\
+n\You can choose 'Canberra', 'Queanbeyan', or 'Sydney'")
+        addStation()
+
+
+# Question 2 Functions----------------------
+def validClimate(climate):
+    climates = ["wet", "dry"]
+    return isValid(climates, climate)
+
+# Question 3 Functions----------------------
+def validDiffValue(value):
+    if len(value) > 1:
+        for character in value:
+            if not ord("0") <= ord(character) <= ord("9"):
+                return False
+    else:
+        if value[0] == "0":
+            return False
+    return True
+
+# Question 4 Functions----------------------
+def validDiffUnit(value):
+    units = ["days", "months", "years"]
+    return isValid(units, value)
+
+# Question 5 Functions----------------------
+monthList = ["jan", "feb", "mar", "apr", "may", "jun",
+               "jul", "aug", "sep", "oct", "nov", "dec",
+               "january", "feburary", "march", "april",
+               "may", "june", "july", "august", "september",
+               "october", "november", "december"]
+def convertDigitToMonthName(digit):
+    names = monthList[:]
+    name = names[digit+11].title()
+    return name
+
+def convertMonthToDigit(month):
+    months = monthList[:]
+    index = months.index(month)
+    monthNumber = index%12 + 1
+    if monthNumber == 0:
+        monthNumber = 12
+    return monthNumber
+
+def validTP_Unit(value):
+    B1units = monthList[:]
+    B2units = ["month", "year"]
+             
+    if isValid(B1units, value):
+        return [True, "B1units"]
+    if isValid(B2units, value):
+        return [True, "B2units"]
+    if validDiffValue(value):
+        if 0 <= int(value) <= 12:
+            return [True, "B1unitsDigit"]
+    return [False]
+
+# Question 6 Functions----------------------
+def validOcc(occ):
+    units = ["dates", "months", "years"]
+    return isValid(units, occ)
+
+# Question 7 Functions----------------------
+def answerToBool(answer):
+    if "y" in answer.lower():
+        return True
+    return False
+
+def validAnswer(answer):
+    answers = ["yes", "no", "y", "n"]
+    if isValid(answers, answer):
+        return [True, answerToBool(answer)]
+    return [False]
+
+def validRedo(number):
+    if len(number) > 1:
+        return False
+    if not ord("1") <= ord(number) <= ord("6"):
+        return False
+    return True
+
+def redoThis(number):
+    if number == 1:
+        WeatherStations.clear()
+        question1()
+    if number == 2:
+        WetOrDry.clear()
+        climatePrint.clear()
+        climatePrint.append("driest period")
+        question2()
+    if number == 3:
+        DifferenceValue.clear()
+        question3()
+    if number == 4:
+        DifferenceUnit.clear()
+        question4()
+    if number == 5:
+        TimePeriod.clear()
+        TimePeriodPrint.clear()
+        question5()
+    if number == 6:
+        Occurences.clear()
+        question6()
+
+def needsToBeFixed():
+    print("Oh no! \n\
+was it the: \n\
+1) Weather Staions\n\
+2) Climate\n\
+3) 'Once in a ________' value?\n\
+4) 'Once in a ________' unit?\n\
+5) The time period (whether we're looking at a specific month, or justs days \
+or years)\n\
+6) When this event occurs (dates or years)")
+    needsFixing = input("Enter in the number which corresponds to what needs \
+fixing:\n")
+    if validRedo(needsFixing):
+        redoThis(int(needsFixing))
+        question7()
+    else:
+        print("That's not a number between 1 and 6!!! >___<\n")
+        needsToBeFixed()
+
+
+# Questions
+def question1():
+    AmountOfWeatherStations = input("Please enter the number of Weather \
+Stations you would like info from: ")
+    if validAmountOfStations(AmountOfWeatherStations):
+        addStation()
+        for x in range(int(AmountOfWeatherStations) -1):
+            print("Ready for you to type the name of your next Station!")
+            addStation()
+    else:
+        print("Sorry, I think", AmountOfWeatherStations, 
+              "isn't between 1 and 3")
+        question1()
+
+def question2():
+    WetDry = input("Please type 'Wet' or 'Dry': ")
+    if validClimate(WetDry):
+        WetOrDry.append(WetDry.lower())
+        climate = WetOrDry[:]
+        if climate[0] == "wet":
+            climatePrint[0] = "rainfall"
+    else:
+        print("Sorry, I don't think you typed 'Wet' or 'Dry'")
+        question2()
+
+def question3():
+    diff = input("What is a once in ____________ days/months/years "+ 
+                 climatePrint[0] +" time. \n")
+    if validDiffValue(diff):
+        DifferenceValue.append(int(diff))
+    else:
+        print("I maybe wasn't very clear hahaha, \
+but your input should be a number in digit form :)\n\nFill in the Blank: ")
+        question3()
+
+def question4():
+    diffUnit = input("What is a once in "+ str(DifferenceValue[0]) +
+                     " ____________________ "+ climatePrint[0] +" time. \n")
+    if validDiffUnit(diffUnit):
+        DifferenceUnit.append(diffUnit)
+    else:
+        print("I maybe wasn't very clear hahaha, \
+your units can be 'days', 'months', or 'years' ^_^\n\nFill in the Blank:")
+        question4()
+        
+def question5():
+    month = 0
+    TP_unit = input("What is a once in "+ str(DifferenceValue[0]) +" "\
+                     + str(DifferenceUnit[0]) +" "+ climatePrint[0] +\
+                     " for the month of ___________________ \n\n\
+What is a once in "+ str(DifferenceValue[0]) +" "\
+                     + str(DifferenceUnit[0]) +" "+ climatePrint[0] +\
+                     " in a ___________________ \n")
+    temp = validTP_Unit(TP_unit)[:]
+    if temp[0]:
+        if "B1units" in temp[1]:
+            if not "Digit" in temp[1]:
+                month = convertMonthToDigit(TP_unit)
+            else:
+                month = int(TP_unit)
+                
+            TimePeriod.append("month")
+            TimePeriod.append(month)
+            TimePeriodPrint.append("for the month of "+
+                                   convertDigitToMonthName(month))
+        else:
+            TimePeriod.append(TP_unit)
+            TimePeriodPrint.append("in a single "+TP_unit)
+    else:
+        print("\n\nI maybe wasn't very clear hahaha.\n\
+If you opted for the first blank, this should be a month name or number.\n\
+If you opted for the second blank, this should either be 'month', \
+or 'year'")
+        question5()
+        
+def question6():
+    Occ = input("What is a once in "+ str(DifferenceValue[0]) +" "\
+                + str(DifferenceUnit[0]) +" "+ climatePrint[0] +\
+                " "+ str(TimePeriodPrint[0]) + ", and on which __________\
+ did this occur?\n")
+    if validOcc(Occ):
+        Occurences.append(Occ)
+    else:
+        print("I maybe wasn't very clear hahaha, \
+either type in 'dates', 'months', or 'years' :)\n\nFill in the Blank:")
+        question6()
+    
+def question7():
+    stations = WeatherStations[:]
+    stationString = ""
+    index = 0
+    while index < len(stations):
+        if len(stations) > 1:
+            if not index == len(stations) -1:
+                stationString += stations[index].title() + ", "
+            else:
+                stationString += ("and "+stations[index].title() + 
+                " weather stations")
+        else:
+            stationString = stations[index].title() + " weather station"
+        index += 1
+    
+    print("You want to know: \n\
+What is a once in "+ str(DifferenceValue[0]) +" "\
++ str(DifferenceUnit[0]) +" "+ climatePrint[0] +\
+" "+ str(TimePeriodPrint[0]) + ", and on which "+\
+str(Occurences[0]) + " did this occur?\n\
+For the " + stationString+ ", right? :)")
+    answer = input("Enter 'yes' or 'no' \n")
+    
+    tempAnswer = validAnswer(answer)[:]
+    if tempAnswer[0]:
+        if tempAnswer[1]:
+            print("Yay!")
+        else:
+            needsToBeFixed()
+    else:
+        print("Please enter in either 'yes' or 'no'. \
+What you wanna solve matters to me <3")
+        question7()
+def main():  
+    print ("Hi!!! \n\
+This program lets you get the stats for \n\
+'once in *however many* days/months/years' events \n\
+for rainfall in Canberra, Queanbeyan and Sydney. \n\
+So to help you out, we'll need to ask for some input from you \n\
+so we can figure give back exactly what you wanted. \n\n\
+Alrighty! \n\
+First question: How many Weather Stations would you like to get info from?")
+    question1()
+    
+    print("\n\nSecond Question: Would you like to find the Wettest or Dryiest \
+period?")
+    question2()
+    
+    print("\n\nThird Question: Fill in the blank:")
+    question3()
+    
+    print("\n\nFourth Question: Fill in the blank:")
+    question4()
+    
+    print("\n\nFifth Question: Fill in one of the blanks:")
+    question5()
+    
+    print("\n\nSixth Question: Fill in the blank:")
+    question6()
+    
+    print("\n\nLast Question: Does all of this info sound right to you? :)")
+    question7()
+    
+    runMiles(WeatherStations, WetOrDry[0], int(DifferenceValue[0]), 
+             DifferenceUnit[0], TimePeriod, Occurences[0])
+main()
---
+++ assignmen_code2.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 12 16:22:08 2017
+
+@author: miles
+"""
+import numpy as np
+import matplotlib.pyplot as mpl
+import csv
+import alfred.py
+#fileobj = open("Rainfall_Sydney_066062.csv","r")
+
+
+
+def build_list(f):
+    fileobj = open(f,"r")
+    reader = csv.reader(fileobj)
+    data = []
+    flag = 0
+
+    for i in reader:
+        data.append(i)
+    for j in data:
+        if j[5] != '' and j[6] == '':
+           # print("hello",j)
+            j[6] = 1
+        if j[5] == '' and j[6] == '':
+            j[6] = 0
+            j[5] = 0
+    
+    data.pop(0)
+    
+    return alfred.avgRainAccrossDays(data)
+
+data = build_list("Rainfall_Sydney_066062.csv")
+def sub_lst(l,yr,mnt): 
+    ll = [x for x in l if x[2] == yr and x[3] == mnt]
+    total = len(ll)
+    #print(ll)
+    summ = sum([int(y[6]) for y in ll])
+    sumr = sum([float(y[5]) for y in ll])
+    #print("the total is ",total,"the sum is ",summ)
+    if summ != 28 and summ != 30 and summ != 31:
+        #print("invalid month")
+        aaa=1
+    
+    flag = 0
+    mnt_i = int(mnt)
+    if (( mnt_i in [1,3,5,7,8,10,12] and summ == 31) or (mnt_i in [4,6,9,11] and summ==30) or (mnt_i==2 and summ==28)) and (int(yr)%4 != 0):
+        #print("This is a valid month")
+        flag = 1
+    elif (( mnt_i in [1,3,5,7,8,10,12] and summ == 31) or (mnt_i in [4,6,9,11] and summ==30) or (mnt_i==2 and summ==29)) and (int(yr)%4 == 0):
+        #print("This is a valid month")
+        flag = 1
+    else:
+        #print("This is NOT a valid month")
+        aaa=1
+    
+    if flag == 1:
+        stt = (yr,mnt,sumr)
+    else:
+        stt = (yr,mnt,-1)
+    return stt
+
+
+
+def Derive_Month_List(l):
+    years = []
+    months = ['01','02','03','04','05','06','07','08','09','10','11','12']
+    lst = []
+    flag = 0
+    for i in l:
+        if i[2] not in years:
+            years.append(i[2])
+            
+    for k in years:
+        flag = 0
+        for m in range(0,12):
+            #print("this is flag ",flag,lst)
+            obj = sub_lst(l,k,months[m])
+            lst.append(obj)        
+    return(lst)
+     
+F = Derive_Month_List(data)
+
+def Derive_Specific_Mnth_List(W,mnth):
+    Lst = [x for x in  W  if x[1]==mnth ]
+    return Lst
+
+
+    
+def one_year(l,yr):
+    lst = []
+    point = [x for x in l if x[0] == yr]
+    print(point)
+    for i in point:
+        proxy = i[2]
+        if proxy != -1:
+            #lst.append(i[0])
+            lst.append(proxy)
+        else:
+            print("invalid month")
+    return lst
+
+#print(one_year(F,'2017'))
+def year_sum(l,yr):
+    yrs = [x for x in l if x[2] == yr]
+    summ = 0
+    for i in yrs:
+        summ = summ + float(i[5])
+    return summ    
+
+
+
+def total_year_list(l,dt):
+    lst = []
+    years = []
+    s = []
+    print("this is l ",l)
+    flag = 0
+    months = ['01','02','03','04','05','06','07','08','09','10','11','12']
+    #print()
+    for i in l:
+        if i[0] not in years:
+            years.append(i[0])
+    for k in years:
+        #print("this is k",k)        
+        tot = year_sum(dt,k)
+        s.append((k,tot))
+#        for m in range(0,12):
+#            #if l[flag][2] != -1:
+#           summ = summ + float(dt[flag][5])
+#        #h.append(years[])
+#        h.append(summ)
+#        flag += 1
+#        lst.append(h)
+    #print("these are the years ",years)
+#    print("the total sum is ",len())    
+#    print("the total years are ",len(years))
+    print("ddffgg ",s)
+    return s
+
+
+
+def main_loop(WeatherStations, WetOrDry,DifferenceValue, DifferenceUnit, TimePeriod, Occurences):
+    lst = build_list(WeatherStations)
+    F = Derive_Month_List(lst)
+    if ():
+        data = one_year(F,TimePeriod[1])
+    if ():    
+        data = total_year(F)
+    s_data = sort(data)
+    m1 = method1(DifferenceUnit,s_data)
+
+
+
+
+def check_valid_year(l,yr):
+    ll = [x for x in l if x[2]]
+    i = 0
+    while ll[i][6] == 0:
+        i = i+1
+    fstfnd = i
+#    if (fstfnd+1) != ll[i][6]:
+        
+
+    
+def sort(l):
+    y = []
+    b = 0
+    for i in l:
+        #print("i is ",i)
+        st = (i,b)
+            #print(st)
+        y.append(st)
+        b = b+1 
+        #print("b is ",b)
+    #print(type(y))  
+    #print(y)
+    y.sort(key=lambda tup: float(tup[0]))     
+    return y
+        
+    
+    
+    
+def method1(x,l):
+    ln = len(l)
+    print(l)
+    for i in range(0,ln-2):
+        #print("hi this is i ",i,ln)
+        flag = 1
+        for j in range(i,ln-1):
+            if j%1000 == 0:
+                print('i=',i,' j=',j)
+            if flag == 0:
+                break
+            for k in range(j+1,ln):
+                #print(" j = ",l[j],"k = ",l[k],"the difference is ",abs(l[j][1] - l[k][1]))
+                if abs(l[j][1] - l[k][1]) < x:
+                    #print(" j = ",l[j],"k = ",l[k],"the difference is ",abs(l[j][1] - l[k][1]))
+                    flag = 0
+                    break
+        if flag == 1:
+            return l[i]     
+    
+    
+
+
+#-----------------------------------------------------------------------
+# THIS IS THE MAINLINE OF THE PROGRAM 
+
+print("\n\n")
+print("Welcome to our Weather analysis program")
+print("Please wait while I analyse the data")
+print("This program is written by Miles Pennifold and Alfred Le")
+
+
+
+F = Derive_Month_List(data)
+print(F[0:10])
+print("higgs ",one_year(F,'1998'))        
+
+print("")
+        
+G = Derive_Specific_Mnth_List(F,'01')
+for i in range(0,10):
+    print(G[i])
+
+
+#print("hi this is the total year list ",total_year_list(F,data))            
+
+#years = [int(x[0])   for x in G if x[2] != -1]   
+#y =     [float(x[2]) for x in G if x[2] != -1]    
+years = [int(x[0])   for x in F if x[2] != -1]   
+y =     [float(x[2]) for x in F if x[2] != -1]    
+
+
+
+
+#years = [1968, 1969, ..., 2016, 2017] # list of years in the table above
+#y = [28.7, 56.0, ..., 148.3, 0.4] # corresponding list of total rainfall values
+x = np.arange(0, len(y))   
+mpl.plot(x, y, '.b', markersize=5)
+mpl.plot([0, len(y) + 1], [143.8, 143.8], '--k')
+mpl.xticks(x, years, rotation=90)
+#mpl.grid(color='r', linestyle='-', linewidth=2)
+#mpl.grid()
+mpl.show()
+
+#avg_month_time(data,'06')    
+
+#print(sub_lst(data,'1858','04'))    
