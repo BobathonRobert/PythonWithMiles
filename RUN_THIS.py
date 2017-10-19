dd3c927a794ccade8f14eeb88470f001955ff809
Bobathon Robert <pimanhappy@gmail.com>
2017-10-19 13:41:02 +1100

    minor text fixes

--- RUN_THIS.py
+++ RUN_THIS.py
@@ -5,7 +5,10 @@ Created on Thu Oct 19 13:27:51 2017
 @author: u6049237 -0487201989
 """
 
-import miles.py
+import miles
 
 # Made this do it'd be clear on how to get the program running
+
+print("Welcome to our Weather analysis program\n\
+This program is written by Alfred Le and Miles Pennifold\n")
 miles.main_loop_with_parameters()
